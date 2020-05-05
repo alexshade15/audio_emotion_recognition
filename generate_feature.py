c02a5e9f5845ec3533396280d1103ce2900e9cdf
Alex15 <alexshade@live.it>
2020-05-05 19:11:28 +0200

    fix feature extraction

--- generate_feature.py
+++ generate_feature.py
@@ -4,10 +4,19 @@ from os import listdir, path
 
 def to_milliseconds(t_stamp):
     t = t_stamp.split(":")
-    h = int(t[0])
+    # h = int(t[0])
     m = int(t[1])
     s_ms = t[2].split(".")
-    return int(s_ms[1]) * 10 + int(s_ms[0]) * 1000 + m * 6000 + h * 360000
+    return int(s_ms[1]) * 10 + int(s_ms[0]) * 1000 + m * 6000  # + h * 360000
+
+
+def to_t_stamp(milliseconds):
+    s = milliseconds // 6000
+    milliseconds = milliseconds % 6000
+    string_sec = str(s)
+    if len(string_sec) == 1:
+        string_sec = "0" + string_sec
+    return "00:00:" + string_sec + "." + str(milliseconds)
 
 
 def generate_files(base_dir):
@@ -27,6 +36,8 @@ def get_avi_info():
         print(base_dir + dataset + "/" + emotion + "/" + name, "\n\n\n")
 
 
+# ffmpeg -y -i /user/vlongobardi/AFEW/videos/Train/Angry/000046280.avi 2>&1 | grep Duration | awk '{print $2}' | tr -d ,
+# ffmpeg -ss 0 -t 0.3 -i /user/vlongobardi/AFEW/videos/Train/Angry/000046280.avi -ab 128k -ac 2 -ar 48000 -vn 000046280_0.wav
 def from_avi_to_wav():
     base_dir = "/user/vlongobardi/AFEW/videos/"
     wav_dir = "/user/vlongobardi/wav/"
@@ -37,17 +48,25 @@ def from_avi_to_wav():
         bash_output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         duration = to_milliseconds(bash_output.stderr.decode("utf-8").split("Duration: ")[1].split(",")[0])
         i = 0
-        d0 = 0
+        d0 = frame_step
+
+        command = "ffmpeg -t " + to_t_stamp(frame_size) + " -i " + base_dir + dataset + "/" + emotion + "/" + name + \
+                  ".avi -ab 128k -ac 2 -ar 48000 -vn " + wav_dir + dataset + "/" + emotion + "/" + name + "_" + str(i) \
+                  + ".wav"
+        subprocess.call(command, shell=True)
+
         while d0 < (duration - frame_size):
-            command = "ffmpeg -ss " + str(d0) + " -t " + str(d0 + frame_size) + " -i " + base_dir + dataset + "/" + emotion + "/" + name + ".avi -ab 128k -ac 2 -ar 48000 -vn " + wav_dir + dataset + "/" + emotion + "/" + name + "_" + str(i) + ".wav"
-            subprocess.call(command, shell=True)
-            print(base_dir + dataset + "/" + emotion + "/" + name + "_" + str(i), "\n\n\n")
             i += 1
+            command = "ffmpeg -ss " + to_t_stamp(d0) + " -t " + to_t_stamp(frame_size) + " -i " + base_dir + dataset + \
+                      "/" + emotion + "/" + name + ".avi -ab 128k -ac 2 -ar 48000 -vn " + wav_dir + dataset + "/" + \
+                      emotion + "/" + name + "_" + str(i) + ".wav"
+            subprocess.call(command, shell=True)
             d0 += frame_step
         if d0 < duration:
-            command = "ffmpeg -ss " + str(duration - frame_size) + " -t " + str(duration) + " -i " + base_dir + dataset + "/" + emotion + "/" + name + ".avi -ab 128k -ac 2 -ar 48000 -vn " + wav_dir + dataset + "/" + emotion + "/" + name + "_" + str(i) + ".wav"
+            command = "ffmpeg -ss " + to_t_stamp(duration - frame_size) + " -t " + to_t_stamp(frame_size) + " -i " + \
+                      base_dir + dataset + "/" + emotion + "/" + name + ".avi -ab 128k -ac 2 -ar 48000 -vn " + wav_dir \
+                      + dataset + "/" + emotion + "/" + name + "_" + str(i+1) + ".wav"
             subprocess.call(command, shell=True)
-            print(base_dir + dataset + "/" + emotion + "/" + name + "_" + str(i), "\n\n\n")
 
 
 def from_wav_to_feature():
@@ -60,6 +79,5 @@ def from_wav_to_feature():
         print(name, "\n\n\n")
         subprocess.call(command, shell=True)
 
-
-#from_avi_to_wav()
-#from_wav_to_feature()
+# from_avi_to_wav()
+# from_wav_to_feature()
