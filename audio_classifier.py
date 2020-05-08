87ee0a38f5ba98ace951eb2ea6e55110546a0a90
Alex15 <alexshade@live.it>
2020-05-08 12:15:54 +0200

    code refactor

--- audio_classifier.py
+++ audio_classifier.py
@@ -3,7 +3,7 @@ import csv
 import random
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dropout, Dense, Flatten
+from keras.layers import Dropout, Dense
 from sklearn.preprocessing import LabelBinarizer
 from keras.callbacks import TensorBoard
 from keras.optimizers import Adam, SGD
@@ -29,32 +29,32 @@ def get_labels(path):
 
 
 def from_arff_to_feture(arff_file):
-    #print("\n\n\narff_file", arff_file)
+    # print("\n\n\narff_file", arff_file)
     with open(arff_file, 'r') as f:
         arff = f.read()
-        #print("\n\n\narff", arff)
+        # print("\n\n\narff", arff)
         header, body = arff.split("@data")
-        #print("\n\n\nbody", body.encode('utf-8'), "--")
+        # print("\n\n\nbody", body.encode('utf-8'), "--")
         try:
-          features = body.split(",")
-          features.pop(0)  # remove name
-          features.pop(-1)
+            features = body.split(",")
+            features.pop(0)  # remove name
+            features.pop(-1)
         except:
-          print("\n\n", arff_file, "\n\n")
+            print("\n\n", arff_file, "\n\n")
     return features
 
 
 def get_all_arff(path):
     arffs = []
-    dirs = os.listdir(path)
-    for dir in dirs:
-        sub_arff = os.listdir(path + "/" + dir)
+    folders = os.listdir(path)
+    for folder in folders:
+        sub_arff = os.listdir(path + "/" + folder)
         for arff in sub_arff:
-            arffs.append(dir + "/" + arff)
+            arffs.append(folder + "/" + arff)
     return arffs
 
 
-def data_gen(feature_folder, list_feature_vectors, batch_size, mode="train", feature_number=384):
+def data_gen(feature_folder, list_feature_vectors, batch_size, feature_number=1582, mode="train"):
     lbs = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
     lb = LabelBinarizer()
     lb.fit_transform(np.array(lbs))
@@ -74,23 +74,23 @@ def data_gen(feature_folder, list_feature_vectors, batch_size, mode="train", fea
             random.shuffle(list_feature_vectors)
             if mode == "eval":
                 break
-        #print("\n\nLABEL", labels)
+        # print("\n\nLABEL", labels)
         labels = lb.transform(np.array(labels))
-        #print("\n\nLABEL", labels)
+        # print("\n\nLABEL", labels)
         yield features, labels
 
 
-def train_model(train_path, val_path, batch_size, epochs, lr, feature_number=384):
+def train_model(train_path, val_path, batch_size, epochs, learning_rate, feature_number=1582):
     print(feature_number, "\n\n")
     model = Sequential()
-    model.add(Dense(16, input_shape=(feature_number, ), activation='relu'))
-    #model.add(Dropout(0.5))
+    model.add(Dense(16, input_shape=(feature_number,), activation='relu'))
+    # model.add(Dropout(0.5))
     model.add(Dense(32, activation='relu'))
     model.add(Dense(64, activation='relu'))
-    #model.add(Dropout(0.5))
+    # model.add(Dropout(0.5))
     model.add(Dense(7, activation='softmax'))
 
-    model.compile(optimizer=Adam(learning_rate=lr), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(learning_rate=learning_rate), loss='categorical_crossentropy', metrics=['accuracy'])
     model.summary()
 
     train_files = get_all_arff(train_path)
@@ -100,7 +100,7 @@ def train_model(train_path, val_path, batch_size, epochs, lr, feature_number=384
     no_of_training_images = len(train_files)
     no_of_val_images = len(val_files)
 
-    #tb_call_back = TensorBoard(log_dir="logs_audio", write_graph=True, write_images=True)
+    # tb_call_back = TensorBoard(log_dir="logs_audio", write_graph=True, write_images=True)
     history = model.fit_generator(train_gen, epochs=epochs, steps_per_epoch=(no_of_training_images // batch_size),
                                   validation_data=val_gen, validation_steps=(no_of_val_images // batch_size))
     #                              callbacks=[tb_call_back])
@@ -111,10 +111,9 @@ def train_model(train_path, val_path, batch_size, epochs, lr, feature_number=384
     print("Val Loss:\n", history.history['val_loss'])
 
 
-batch_size = 16
-epochs = 50
+bs = 16
+ep = 50
 lr = 0.01
 base_path = "/user/vlongobardi/audio_feature_IS09/"
-feature_number = get_feature_number(base_path.split("/")[-2])
-print("epochs:", epochs, "batch_size:", batch_size, "lr:", lr)
-train_model(base_path + "Train", base_path + "Val", batch_size, epochs, lr, feature_number)
+print("epochs:", ep, "batch_size:", bs, "lr:", lr)
+train_model(base_path + "Train", base_path + "Val", bs, ep, lr, get_feature_number(base_path.split("/")[-2]))
