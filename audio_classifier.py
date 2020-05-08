bf4f7ae6782ae1b794edce09d5795a7fa0752794
Vincenzo Longobardi <vlongobardi@dldev1.diem.unisa.it>
2020-05-08 12:09:11 +0200

    get_feature_number

--- audio_classifier.py
+++ audio_classifier.py
@@ -9,6 +9,14 @@ from keras.callbacks import TensorBoard
 from keras.optimizers import Adam, SGD
 
 
+def get_feature_number(feature_name):
+    if feature_name == "audio_feature_IS09":
+        return 384
+    if feature_name == "audio_feature_emobase2010":
+        return 1582
+    return None
+
+
 def get_labels(path):
     csvfile = open(path, 'rb')
     reader = csv.reader(csvfile)
@@ -46,7 +54,7 @@ def get_all_arff(path):
     return arffs
 
 
-def data_gen(feature_folder, list_feature_vectors, batch_size, mode="train"):
+def data_gen(feature_folder, list_feature_vectors, batch_size, mode="train", feature_number=384):
     lbs = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
     lb = LabelBinarizer()
     lb.fit_transform(np.array(lbs))
@@ -55,12 +63,10 @@ def data_gen(feature_folder, list_feature_vectors, batch_size, mode="train"):
         random.shuffle(list_feature_vectors)
     while True:
         labels = []
-        features = np.zeros((batch_size, 1582)).astype('float')
+        features = np.zeros((batch_size, feature_number)).astype('float')
         for i in range(c, c + batch_size):
             feature = from_arff_to_feture(feature_folder + "/" + list_feature_vectors[i])
-            # print(len(feature), list_feature_vectors[i])
-            feature = np.array(feature) #.reshape(1, 1582)
-            features[i - c] = feature
+            features[i - c] = np.array(feature)
             labels.append(list_feature_vectors[i].split("/")[0])
         c += batch_size
         if c + batch_size - 1 > len(list_feature_vectors):
@@ -74,12 +80,14 @@ def data_gen(feature_folder, list_feature_vectors, batch_size, mode="train"):
         yield features, labels
 
 
-def train_model(train_path, val_path, batch_size, epochs, lr):
+def train_model(train_path, val_path, batch_size, epochs, lr, feature_number=384):
+    print(feature_number, "\n\n")
     model = Sequential()
-    model.add(Dense(32, input_shape=(1582, ), activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dense(16, input_shape=(feature_number, ), activation='relu'))
+    #model.add(Dropout(0.5))
+    model.add(Dense(32, activation='relu'))
     model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
+    #model.add(Dropout(0.5))
     model.add(Dense(7, activation='softmax'))
 
     model.compile(optimizer=Adam(learning_rate=lr), loss='categorical_crossentropy', metrics=['accuracy'])
@@ -87,8 +95,8 @@ def train_model(train_path, val_path, batch_size, epochs, lr):
 
     train_files = get_all_arff(train_path)
     val_files = get_all_arff(val_path)
-    train_gen = data_gen(train_path, train_files, batch_size=batch_size)
-    val_gen = data_gen(val_path, val_files, batch_size=batch_size)
+    train_gen = data_gen(train_path, train_files, batch_size, feature_number)
+    val_gen = data_gen(val_path, val_files, batch_size, feature_number)
     no_of_training_images = len(train_files)
     no_of_val_images = len(val_files)
 
@@ -102,8 +110,11 @@ def train_model(train_path, val_path, batch_size, epochs, lr):
     print("\n\nTrain Loss:\n", history.history['loss'])
     print("Val Loss:\n", history.history['val_loss'])
 
+
 batch_size = 16
 epochs = 50
 lr = 0.01
+base_path = "/user/vlongobardi/audio_feature_IS09/"
+feature_number = get_feature_number(base_path.split("/")[-2])
 print("epochs:", epochs, "batch_size:", batch_size, "lr:", lr)
-train_model("/user/vlongobardi/audio_feature/Train", "/user/vlongobardi/audio_feature/Val", batch_size, epochs, lr)
+train_model(base_path + "Train", base_path + "Val", batch_size, epochs, lr, feature_number)
