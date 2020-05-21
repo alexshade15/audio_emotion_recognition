c24e02b35d6496039608766fa273798028fc38a6
Alex15 <alexshade@live.it>
2020-05-21 14:23:30 +0200

    new models

--- test_models_for_audio.py
+++ test_models_for_audio.py
@@ -1,8 +1,8 @@
-from keras.models import Sequential
-from keras.layers import Dropout, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dropout, Dense
 
 
-def model1(feature_number):  # 6,635
+def model1(feature_number=384):  # 6,635 // 1,843
     model = Sequential()
     model.add(Dense(4, input_shape=(feature_number,), activation='relu'))
     model.add(Dense(8, activation='relu'))
@@ -11,7 +11,7 @@ def model1(feature_number):  # 6,635
     return model
 
 
-def model2(feature_number):  # 13,583
+def model2(feature_number=384):  # 13,583 // 3,999
     model = Sequential()
     model.add(Dense(8, input_shape=(feature_number,), activation='relu'))
     model.add(Dense(16, activation='relu'))
@@ -20,7 +20,7 @@ def model2(feature_number):  # 13,583
     return model
 
 
-def model3(feature_number):  # 26,103
+def model3(feature_number=384):  # 26,103 // 6,935
     model = Sequential()
     model.add(Dense(16, input_shape=(feature_number,), activation='relu'))
     model.add(Dense(32, activation='relu'))
@@ -28,11 +28,71 @@ def model3(feature_number):  # 26,103
     return model
 
 
-def model4(feature_number):  # 12,943
+def model4(feature_number=384):  # 12,943 // 3,359
     model = Sequential()
     model.add(Dense(8, input_shape=(feature_number,), activation='relu'))
     model.add(Dense(8, activation='relu'))
     model.add(Dense(8, activation='relu'))
     model.add(Dense(8, activation='relu'))
+    model.add(Dense(7, activation='softmax'))
+    return model
+
+
+def model5(feature_number=384):  # 104,119 // 27,447
+    model = Sequential()
+    model.add(Dense(64, input_shape=(feature_number,), activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(7, activation='softmax'))
+    return model
+
+
+def model5_1(feature_number=384):  # 102,759 // 27,367
+    model = Sequential()
+    model.add(Dense(64, input_shape=(feature_number,), activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(7, activation='softmax'))
+    return model
+
+
+def model5_2(feature_number=384):  # 51,303 // 12,967
+    model = Sequential()
+    model.add(Dense(32, input_shape=(feature_number,), activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(7, activation='softmax'))
+    return model
+
+
+def model6(feature_number=384):  # 15,759 // 6,335
+    model = Sequential()
+    model.add(Dense(8, input_shape=(feature_number,), activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(7, activation='softmax'))
+    return model
+
+
+def model6_1(feature_number=384):  # 24,687 // 15,103
+    model = Sequential()
+    model.add(Dense(8, input_shape=(feature_number,), activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(7, activation='softmax'))
+    return model
+
+
+def model6_2(feature_number=384):  # 58,607 // 49,023
+    model = Sequential()
+    model.add(Dense(8, input_shape=(feature_number,), activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(256, activation='relu'))
     model.add(Dense(7, activation='softmax'))
     return model
